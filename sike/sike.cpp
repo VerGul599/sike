c0f78cefca98fc3d7a2324a37a3295f934dbd974
Вероника <@WIN-BBNSI5FE1UN>
2024-03-11 21:41:52 +0500

    Добавьте файлы проекта.

---
+++ sike/sike.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+    int k = 7;
+    for (int i = 1; i <= 10; i++)
+    {
+        std::cout << k << " x " << i << " = " << k * i << "\n";
+    }
+
+    return 0;
+}
